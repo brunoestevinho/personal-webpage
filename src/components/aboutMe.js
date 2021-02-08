3f6a1ff357a225e75dc3e0d0bc14706a7e21852a
Bruno Estevinho <bruno.m.estevinho@gmail.com>
2021-02-08 22:29:07 +0000

    style fix

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -3,10 +3,8 @@ import React from "react";
 const AboutMe = () => {
   return (
     <section className="text-white pt-6 pb-20" id="about">
-      <header>
-        <h2 className="text-4xl font-bold pb-12">The Boring stuff</h2>
-      </header>
-      <p className="px-4 text-center">
+      <h2 className="text-4xl font-bold pb-12">The Boring stuff</h2>
+      <p className="px-4 text-center pb-12">
         I'm from Portugal and i have concluded my master degree in
         Eletrotecnical and Computers Engineering. I had my first job before
         finishing my studies as a Support Specialist. I stayed for more than 2
@@ -14,6 +12,8 @@ const AboutMe = () => {
         to pursue a carrear I really enjoyed. I quit my job and started to
         deepen my knowledge in Front-End development.
       </p>
+      <h2 className="text-4xl font-bold pb-12">Skills and tools</h2>
+      <p className="px-4 text-center"></p>
     </section>
   );
 };
