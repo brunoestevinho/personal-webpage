559c4479f147d3222833ec27a60501ec12848f8b
Bruno Estevinho <bruno.m.estevinho@gmail.com>
2021-02-22 23:52:13 +0000

    corrected projects data and favicon

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const Footer = () => (
   <section className="p-12">
-    <footer className="m-6 text-white">
-      <p className="text-center text-sm">
-        Bruno Estevinho, {new Date().getFullYear()}
+    <footer className="m-6 text-white ">
+      <p className="px-4 text-center pb-12">LinkedIn | GitHub</p>
+      <p className="text-center">
+        © Bruno Estevinho, {new Date().getFullYear()}
       </p>
     </footer>
   </section>
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,19 +1,28 @@
 const projectData = [
   {
-    title: "To-do List",
+    title: "Personal Website",
     image: "../images/todo.png",
-    description: "Easily add and remove to-do's with user authentication",
-    tools: ["React", "Auth0"],
+    description:
+      "My personal website where anyone can find a bit more about me and some of the projects i've made",
+    tools: ["React", "TailwindCSS"],
     github: "https://github.com/brunoestevinho/todo-react",
-    link: "https://take-anote.netlify.app/",
+    link: "https://humblify.netlify.app/",
   },
   {
-    title: "To-do List",
+    title: "Humblify",
+    image: "../images/todo.png",
+    description: "A simple user-interface for your spotify data",
+    tools: ["React", "Spotify API", "TailwindCSS"],
+    github: "https://github.com/brunoestevinho/todo-react",
+    link: "https://humblify.netlify.app/",
+  },
+  {
+    title: "2Do",
     image: "../images/todo.png",
     description: "Easily add and remove to-do's with user authentication",
     tools: ["React", "Auth0"],
     github: "https://github.com/brunoestevinho/todo-react",
-    link: "https://take-anote.netlify.app/",
+    link: "https://2dotodo.netlify.app/",
   },
 ];
 
